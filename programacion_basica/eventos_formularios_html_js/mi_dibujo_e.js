fe8d0f283ee1c2bf0d12615ef85d64bf9bba5ea6
William <williamjaber94@gmail.com>
2022-03-16 19:33:22 -0500

    Update

--- programacion_basica/eventos_formularios_html_js/mi_dibujo_e.js
+++ programacion_basica/eventos_formularios_html_js/mi_dibujo_e.js
@@ -3,11 +3,9 @@ var boton = document.getElementById('botoncito');
 boton.addEventListener("click", dibujoPorClick);
 
 var d = document.getElementById("dibujo");
+var ancho = d.width;
 var lienzo = d.getContext("2d");
-var lineas = 30;
-var l = 0
-var yi, xf;
-var xi, yf;
+
 
 function dibujarLinea(color, xincial, yinicial, xfinal, yfinal)
 {
@@ -19,33 +17,31 @@ function dibujarLinea(color, xincial, yinicial, xfinal, yfinal)
   lienzo.closePath();
 }
 
-do
-{
-  l++;
-  yi = 10 * l;
-  xf = 10 * (l + 1);
-  xi = 10 * l;
-  yf = 10 * (l + 1);
-  dibujarLinea("black", 0, yi, xf, 300);
-  dibujarLinea("red", xi, 0, 300, yf);
-  console.log("lineas " + l);
-} while(l < lineas)
-
-dibujarLinea("red", 1, 1, 1, 299);
-dibujarLinea("red", 1, 299, 299, 299);
-
-dibujarLinea("black", 299, 1, 1, 1);
-dibujarLinea("black", 299, 299, 299, 1);
-
-
 function dibujoPorClick()
 {
-  var val = parseInt(texto.value);
-  var lineas = 30;
+  var lineas = parseInt(texto.value);
   var l = 0
   var yi, xf;
   var xi, yf;
+  var espacio = ancho / lineas;
   console.log(texto);
-  alert("funciona " + val);
+  alert("funciona");
 
+  do
+  {
+    l++;
+    yi = espacio * l;
+    xf = espacio * (l + 1);
+    xi = espacio * l;
+    yf = espacio * (l + 1);
+    dibujarLinea("black", 0, yi, xf, (ancho - 1));
+    dibujarLinea("red", xi, 0, (ancho - 1), yf);
+    console.log("lineas " + l);
+  } while(l < lineas)
+
+  dibujarLinea("red", 1, 1, 1, (ancho - 1));
+  dibujarLinea("red", 1, (ancho - 1), (ancho - 1), (ancho - 1));
+
+  dibujarLinea("black", (ancho - 1), 1, 1, 1);
+  dibujarLinea("black", (ancho - 1), (ancho - 1), (ancho - 1), 1);
 }
